898a2263e58cd67fe2c4587313773322c7e40c9f
mjvrmqz <mjvrmqz@gmail.com>
2026-06-03 14:59:36 -0700

    Update outreach_feed_to_ics.py

--- Feeds/Outreach Feed/outreach_feed_to_ics.py
+++ Feeds/Outreach Feed/outreach_feed_to_ics.py
@@ -5,14 +5,14 @@ from datetime import datetime, timezone
 from ics import Calendar, Event
 
 # === CONFIG ===
-NOTION_TOKEN = os.environ.get("NOTION_KEY", "")
+NOTION_KEY = os.environ.get("NOTION_KEY", "")
 DATABASE_ID  = "28d1691964b480a88260e3871d01824c"
 
-if not NOTION_TOKEN:
+if not NOTION_KEY:
     raise RuntimeError("NOTION_KEY environment variable not set.")
 
 HEADERS = {
-    "Authorization": f"Bearer {NOTION_TOKEN}",
+    "Authorization": f"Bearer {NOTION_KEY}",
     "Notion-Version": "2022-06-28",
     "Content-Type": "application/json",
 }
