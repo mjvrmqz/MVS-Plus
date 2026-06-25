3962ae6650c3bfa81896f805e9654a7bcfbf0aa0
mjvrmqz <mjvrmqz@gmail.com>
2026-06-24 18:23:11 -0700

    Deleted And Moved To Own Repo

--- Resources/Editing Software/After Effects/Keyboard Shortcuts/Move Playhead/Move Playhead 1 Sec.jsx
+++
@@ -1,8 +0,0 @@
-app.beginUndoGroup("Move Backward 1.25s");
-
-var comp = app.project.activeItem;
-if (comp && comp instanceof CompItem) {
-    comp.time -= 1.25;
-}
-
-app.endUndoGroup();
--- Resources/Editing Software/After Effects/Keyboard Shortcuts/Smart Rename/Reset.jsx
+++
@@ -1,3 +0,0 @@
-app.settings.saveSetting("NativeKit", "SmartDeleteSkipConfirm", "false");
-app.settings.saveSetting("NativeKit", "SmartRenameSkipConfirmTimeline", "false");
-app.settings.saveSetting("NativeKit", "SmartRenameSkipConfirmProject", "false");
